819ed199422bacc659d432170ab7330349fb06b1
Dainius <pastas4@gmail.com>
2022-01-03 23:52:16 +0100

    Fix several bugs and add workarounds
    Now the repositories are cloned to the staff namespace directly,
    which prevents the issue of running out of personal project space
    (and is much tidier for the one doing the cloning).
    
    Also the default branch is now unprotected to allow developers to
    push to the cloned repositories, plus a few smaller bug fixes.

--- gitlab/3_installKeysAndHooks.py
+++ gitlab/3_installKeysAndHooks.py
@@ -2,6 +2,7 @@ import csv
 import gitlab
 import codegrade
 import re
+import sys
 
 def load_user_data(filename='webhooks.csv'):
     with open(filename) as user_data:
@@ -27,7 +28,7 @@ def sync(access, organization, roster, assignment, student_readable=False):
         if subgroup.full_path == organization["gitlab-group"] + '/' + organization["subgroup-staff"] + '/' + assignment["subgroup"]:
             staff_group = g.groups.get(subgroup.id)
             print("Using staff group: " + staff_group.web_url)
-        if subgroup.full_path == organization["gitlab-group"] + '/' + organization["subgroup-staff"] + '/' + assignment["subgroup"]:
+        if subgroup.full_path == organization["gitlab-group"] + '/' + organization["subgroup-students"] + '/' + assignment["subgroup"]:
             student_group = g.groups.get(subgroup.id)
             print("Using student group: " + student_group.web_url)
     if student_group is None:
@@ -48,24 +49,30 @@ def sync(access, organization, roster, assignment, student_readable=False):
         group_members = group["git_ids"].split()
         print('Processing', reponame,'...')
         try:
-            if reponame not in [ repo.path for repo in me.projects.list(all=True) ]:
+            if reponame not in [ repo.path for repo in staff_group.projects.list(all=True) ]:
                 print(">", "Repository", reponame, "does not exist yet, cloning...")
                 template = None
                 for repo in staff_group.projects.list(include_subgroups=True, all=True):
                     if repo.path == assignment["gitlab-name"]:
                         template = g.projects.get(repo.id, lazy=False)
                         break
+                print(">", "Using template", template.path_with_namespace)
                 if template is None:
                     raise LookupError("Did not find the template to clone, please check the spelling of assignment.gitlab-name!")
-                fork = template.forks.create({'name': reponame, 'path': reponame})
+                fork = template.forks.create({'name': reponame, 'path': reponame, 'namespace': staff_group.full_path})
                 print(">", "Repository", fork.path_with_namespace, "created successfully")
             repo = None
-            for proj in me.projects.list(all=True):
+            for proj in staff_group.projects.list(all=True):
                 if proj.path == reponame:
                     repo = g.projects.get(proj.id, lazy=False)
                     break
             if repo is None:
                 raise LookupError("Did not find the template repository, check whether forking actually worked")
+            if len(repo.protectedbranches.list()) > 0:
+                def_branch = repo.protectedbranches.list()[0].name
+                print(">", "Removing protection from the", def_branch, "branch of repository", repo.path_with_namespace)
+                p_branch = repo.protectedbranches.get(def_branch)
+                p_branch.delete()
             
             for member in group_members:
                 print(">", "Processing collaborators:", member)
@@ -82,12 +89,12 @@ def sync(access, organization, roster, assignment, student_readable=False):
                         e = sys.exc_info()[0]
                         print('>','Error:', e)
                         no_errors += 1
-            if repo.path in [ projects.path for projects in staff_group.projects.list(all=True) ]:
-                print(">", "Staff already owns", reponame)
-            else:
-                print(">", "Adding staff permission to maintain", reponame)
-                repo.share(staff_group.id, gitlab.MAINTAINER_ACCESS)
-                print(">", "Staff can now maintain", reponame)
+            #if repo.path in [ projects.path for projects in staff_group.projects.list(all=True) ]:
+            #    print(">", "Staff already owns", reponame)
+            #else:
+            #    print(">", "Adding staff permission to maintain", reponame)
+            #    repo.share(staff_group.id, gitlab.MAINTAINER_ACCESS)
+            #    print(">", "Staff can now maintain", reponame)
             if student_readable:
                 print(">","Checking if students can read the repo")
                 if repo.path in [ projects.path for projects in student_group.projects.list(all=True) ]:
@@ -102,7 +109,7 @@ def sync(access, organization, roster, assignment, student_readable=False):
                 repo.keys.create({'title': 'codegrade-key', 'key': group['public_key']})
             else:
                 print('>','Deploy key found for', group['name'])
-            if group['payload_url'] not in [ hook.config['url'] for hook in repo.hooks.list() ]:
+            if group['payload_url'] not in [ hook.url for hook in repo.hooks.list() ]:
                 print('>','Adding webhook for', group['name'])
                 repo.hooks.create({'url': group['payload_url'], 'token': group['secret'], 'push_events': 1})
             else:
@@ -115,6 +122,27 @@ def sync(access, organization, roster, assignment, student_readable=False):
             no_errors += 1
     print('\nProcessed',no_groups,'group(s);',no_errors,'error(s).')
 
+# Not used, but can be used for inviting everyone to the student group:
+
+def read_gitlab_ids(in_file):
+    gitlab_ids = {}
+    with open(in_file, 'r') as f:
+        for row in csv.reader(f, delimiter=','):
+            gitlab_ids[row[1]] = row[3]
+    return gitlab_ids
+
+def invite_users():
+    gl_users = read_gitlab_ids("usernames.csv")
+    gl_users = list(gl_users.values())
+    gl_users = gl_users[1:]
+
+    for member in gl_users:
+        if member != "" and member not in [ members.username for members in student_group.members.list(all=True) ]:
+            print("inviting", member)
+            member_id = g.users.list(username=member)[0].id
+            student_group.members.create({'user_id': member_id, 'access_level': gitlab.REPORTER_ACCESS})
+
+# End of unused code
 
 def main():
     with open("secrets.txt", "r") as secretfile:
@@ -133,16 +161,16 @@ def main():
             }
         },
         organization={
-            'codegrade-id': 33,
+            'codegrade-id': 1921,
             'gitlab-group': 'geoscripting-2022',
             'subgroup-staff': 'staff',
             'subgroup-students': 'students'
         },
         roster='webhooks.csv',
         assignment={
-            'codegrade-id': 98,
-            'gitlab-name': 'Exercise_1_Starter',
-            'subgroup': 'exercise-1'
+            'codegrade-id': 11293,
+            'gitlab-name': 'Exercise_2_Starter',
+            'subgroup': 'exercise-2'
         },
         student_readable=False
     )
